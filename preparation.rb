d60112dbe2f1226e2cefa5653799a6f6c064fbaf
Vicente Valencia <vmvalencia@uc.cl>
2021-01-30 14:50:27 -0300

    Fix id generator

--- preparation.rb
+++ preparation.rb
@@ -2,14 +2,20 @@
 
 require 'net/http'
 
-last_poke_id = 0
-poke_id = 0
-poke_ids = Array.new(8) do
-    last_poke_id = poke_id
-    until poke_id != last_poke_id do
-        poke_id = rand(1..151)
+def gen_ids
+    # Generate eight ids in the range 1..151.
+    # Here, an id candidate is generated eight
+    # times, in each 
+    last_poke_ids = []
+    poke_id = 0
+    poke_ids = Array.new(8) do
+        last_poke_ids.push poke_id
+        while last_poke_ids.include? poke_id do
+            poke_id = rand(1..10)
+        end
+        poke_id
     end
-    poke_id
+    poke_ids
 end
 
 # uri = URI('https://pokeapi.co/api/v2/pokemon?limit=1')
