a73914f974c4c2d682bf353968cbb9aa12a14ac8
Vicente Valencia <vmvalencia@uc.cl>
2021-01-31 10:21:05 -0300

    Create Duel class

---
+++ duel.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class Duel
+  def initialize(participants)
+    @participants = participants
+  end
+
+  def duel
+    who_died = -> { @participants.find(&:dead) }
+    dead = nil
+    attacking = @participants[0]
+    defending = @participants[1]
+    turns_played = 0
+
+    until dead
+      # perform the attack
+      attacking.attack(defending)
+
+      # check for a death
+      dead = who_died.call
+
+      # swap who attacks and who defends
+      old_attacking = attacking
+      attacking = defending
+      defending = old_attacking
+
+      turns_played += 1
+    end
+
+    # NOTE: who attacked last is now in the defender var
+    # and viceversa
+    p "#{defending.name} wins the duel against" \
+      "#{attacking.name} after #{turns_played} turns!"
+  end
+end
